4f72a9ea8c86655887a7d76737954e33e81af3d9
Elsa Fluss <elsafluss@gmail.com>
2021-02-21 17:49:23 -0700

    Created app.

--- did-i-take-it/src/App.js
+++ did-i-take-it/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      rand: Math.floor(Math.random() * Math.floor(1118)),
+      pokemon: {},
+      date: ''
+    }
+  }
+
+  getPokemon = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(data => this.setState({ pokemon: data }))
+  }
+  
+  handleClick = (event) => {
+    event.preventDefault()
+    this.getPokemon(this.state.rand)
+    const date = new Date(Date.now()).toLocaleString()
+    this.setState({ date: date })
+  }
+
+  render() {
+    if (!this.state.pokemon.species) {
+      return <div className="home"><button onClick={this.handleClick} className="button">I took my meds, give me my Pokemon</button></div>
+    } else {
+      return (
+        <div className="App">
+        <p>You caught a {this.state.pokemon.name}!</p>
+        <img src={this.state.pokemon.sprites.front_default} alt="sprite" className="sprite"></img>
+        <p>{this.state.date}</p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>Remember to take a screenshot!</p>
+        </div>
+      );
+    }
+  }
 }
 
-export default App;
+export default App
